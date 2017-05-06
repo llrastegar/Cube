e2bbb0a761a49f7cf8a3a67f3a509cc6ee0d8cf2
Linus <llrastegar@users.noreply.github.com>
2017-05-06 15:40:47 -0500

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -1,6 +1,19 @@
 #include "compress.h"
+#include <fstream>
+#include <vector>
+#include <bitset>
 
-
-int main(){
-
+int main() {
+	std::vector<String> lines;
+	std::ifstream myfile;
+	String line;
+	myfile.open ("tfile.txt", std::ios::binary);
+	while (getline (myfile, line) ) {
+		lines.push_back(line);
+		outln(line);
+	}
+	for (std::size_t i = 0; i<line.size(); ++i) {
+		outln(std::bitset<8>(line.c_str()[i]));
+	}
+	myfile.close();
 }

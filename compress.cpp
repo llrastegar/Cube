7b0a98945c519a59579e1f77bb58699b43522dd0
Linus <llrastegar@users.noreply.github.com>
2017-05-06 16:04:40 -0500

    Added binary-string conversion

--- compress.cpp
+++ compress.cpp
@@ -11,15 +11,20 @@ int main() {
 	while (getline (myfile, line) ) {
 		lines.push_back(line);
 		outln(line);
+		for (std::size_t i = 0; i<line.size(); ++i) {
+			//size_t is very similar to an int
+			//stackoverflow.com claims that one should prefer ++i over i++
+			//as long as that ++i/i++ statement is used at the top of a for loop,
+			//just don't worry about it
+			std::bitset<8> a = std::bitset<8>(line[i]); //convert every character to binary, save it in a
+			//note that you could write line.c_str()[i] instead of the more condensed version above
+			out("bits "); outln(a);
+			out("string "); outln((char)std::bitset<8>(a).to_ulong()); //this is confusing, but
+			//all it's doing is converting the bitset to an unsigned long, then to a character
+			//which is what strings are made out of!
+		}
 	}
-	for (std::size_t i = 0; i<line.size(); ++i) {
-		//size_t is very similar to an int
-		//stackoverflow.com claims that one should prefer ++i over i++
-		//as long as that ++i/i++ statement is used at the top of a for loop,
-		//just don't worry about it
-		outln(std::bitset<8>(line.[i])); //convert every character to binary
-		//note that you could write line.c_str()[i] instead of the more condensed version above
-	}
+	
 	myfile.close();
 	//always remember to do this!
 }

055f1c1dee258cf00482121c4d40bea4f1c2f8bc
Linus <llrastegar@users.noreply.github.com>
2017-05-08 17:24:52 -0500

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -48,6 +48,9 @@ public:
 };
 
 int main() {
-	File mp3File("playingaround.mp3");
-	outln(mp3File.accessBinaryStringAtIndex(1890));
+	File mp3File("playingaround.mp3"); //courtesy of Alex Bohm
+	out("Name: "); outln(mp3File.name);
+	outln(mp3File.type);
+	out("Size: "); outln(mp3File.length);
+	outlnend(mp3File.accessBinaryStringAtIndex(1890));
 }

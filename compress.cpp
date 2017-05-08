b2ee2bd3868f1ea28b445841cf3c1aa2a915c9ce
Linus <llrastegar@users.noreply.github.com>
2017-05-08 17:19:46 -0500

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <bitset> //string->binary, probably has other functions
 
-const int CHAR_TO_INT = 48;
+const int CHAR_TO_INT = 48; //if you every convert a character to an integer, you have to subtract this number from it
 
 int filesize(const char* filename) {
 	std::ifstream in(filename, std::ios::binary | std::ios::ate);
@@ -16,28 +16,28 @@ public:
 	String name;
 	String type;
 	long length;
-	char *memblock;
+	char *contents;
 	std::ifstream file;
 	String fullbitset; //not used for now
 	File() : name("") {
-		memblock = new char[0];
+		contents = new char[0];
 	}
 	File(String fname) : name(fname) {
 		file.open(name, std::ios::in|std::ios::binary|std::ios::ate); //maybe not all necessary?
 		length = filesize(name.c_str()); //c_str makes a String or std::string into char*
 		type = name.substr(name.find_last_of("."));
-		memblock = new char [length]; //length may totally blow up with gig sized files
+		contents = new char [length]; //length may totally blow up with gig sized files
 		file.seekg (0, std::ios::beg);
-		file.read (memblock, length);
+		file.read (contents, length);
 		file.close();
 	}
 	~File() {
-		delete[] memblock; //for every "new" there must be a delete or else you clog memory
+		delete[] contents; //for every "new" there must be a delete or else you clog memory
 	}
 	String accessBinaryStringAtIndex(long i) {
 		//long datatype for now, may need to be long long
 		if (i > length) return "Out of bounds!"; //later add a try/catch error statement
-		return ((std::bitset<8>)memblock[i]).to_string(); //cast from char -> bitset<8> -> string
+		return ((std::bitset<8>)contents[i]).to_string(); //cast from char -> bitset<8> -> string
 	}
 	void compress(){
 		outln(name + " compressed");

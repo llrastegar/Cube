f0efe51f40e2942a9bea9ec200654c238fddad8c
Alex Bohm <aabohm7@gmail.com>
2017-05-08 20:04:51 -0500

    fixed stupid sublime mistake

--- compress.cpp
+++ compress.cpp
@@ -9,16 +9,4 @@ int main() {
 	out("Name: "); outln(mp3File.name);
 	out("Type: "); outln(mp3File.type);
 	out("Size: "); outln(mp3File.length);
-<<<<<<< HEAD
-	for(int i=0;i<5;i++)
-		outln(mp3File[i].to_string());
-	//outlnend(mp3File.accessBinaryStringAtIndex(1890));
-=======
-	outlnend(mp3File.accessBinaryStringAtIndex(1890));
-	File txtFile("Ideas.txt");
-	out("Name: "); outln(txtFile.name);
-	out("Type: "); outln(txtFile.type);
-	out("Size: "); outln(txtFile.length);
-	outlnend(txtFile.accessBinaryStringAtIndex(99));
->>>>>>> 310b46c25145f41953100c0693d040c32f1af3d1
 }

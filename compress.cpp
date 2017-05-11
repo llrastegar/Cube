188f579c9214a1f83a47f0ded3e81e80be8854fa
Alex Bohm <aabohm7@gmail.com>
2017-05-11 10:34:55 -0500

    added key class and idea for the algo

--- compress.cpp
+++ compress.cpp
@@ -5,16 +5,20 @@
 
 
 int main() {
+	std::bitset<8> id('a');
+	boost::dynamic_bitset<> value(16);
+	key k(id, value); //the key class
+	k.print();
 	File mp3File("sound.mp3");
 	out("Name: "); outln(mp3File.name);
 	out("Type: "); outln(mp3File.type);
 	out("Size: "); outln(mp3File.length);
 	outlnend(mp3File[1]);
-	mp3File.compress();
+	//mp3File.compress();
 	File txtFile("Ideas.txt");
 	out("Name: "); outln(txtFile.name);
 	out("Type: "); outln(txtFile.type);
 	out("Size: "); outln(txtFile.length);
 	outlnend(txtFile[-2]); //-1 should be last char in file
-	txtFile.compress();
+	//txtFile.compress();
 }
--- compress.h
+++ compress.h
@@ -6,6 +6,7 @@
 //all else should be put into compress.cpp
 //in case we build a class that handles i/o, we should put it into the header
 //and then include all the libraries/headers that we need to.
+#include <boost/dynamic_bitset.hpp>
 
 typedef std::string String;
 
@@ -37,7 +38,22 @@ int filesize(const char* filename) {
 	std::ifstream in(filename, std::ios::binary | std::ios::ate);
 	return in.tellg();
 }
-
+class key
+{
+public:
+	std::bitset<8> id; //8 bit id string
+	boost::dynamic_bitset<> value; //contains the binary value identified by id
+	key(){
+		id = std::bitset<8> (0);
+		value = boost::dynamic_bitset<> (0);
+	}
+	key(std::bitset<8> i, boost::dynamic_bitset<> v): id(i), value(v) {}
+	void print(){ //debug output
+		outln("Key:");
+		out("\t>"); outln(id);
+		out("\t>"); outln(value);
+	}
+};
 class File {
 public:
 	//all public for now
@@ -79,10 +95,11 @@ public:
 			return (std::bitset<8>)contents[length+i]; //contents[-1] should equal last char in file
 	}
 	void compress(){
-		for(long i=0;i<length;i++){
-			out(contents[i]);
+		for(char id=48;id<123;id++){ //limit on the # of keys (range) for now
+			//go through file and check for a binary string equal to that of the id
+				//for each found string compare the bits comming after and push that into the key value
+				//remove the common bits after each id string, leaving the id bits in for decompress
 		}
-		outln("");
 		outln(name + " compressed");
 	}
 	void decompress(){

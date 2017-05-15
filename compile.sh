9610708bbb8bd7a28ce3bfad9c505dd738639783
Linus <llrastegar@users.noreply.github.com>
2017-05-15 16:58:03 -0500

    Updated with prompt for compilation and running

--- compile.sh
+++ compile.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Compiling..."
-g++ -std=c++11 compress.cpp
+echo "Compile or compile and run? (c/r)"
+read r
+if [ "$r" = "c" ]; then
+	echo "Compiling ... "
+	g++ -std=c++11 compress.cpp
+else
+	echo "Compiling ..."
+	g++ -std=c++11 compress.cpp
+	echo "Running ..."
+	./a.out
+fi;

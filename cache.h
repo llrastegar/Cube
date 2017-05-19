8ac0e22965a71d8237a2832ef17ac19dfbe396df
Linus <llrastegar@users.noreply.github.com>
2017-05-19 16:45:30 -0500

    Original algorithm was flawed

--- cache.h
+++ cache.h
@@ -5,72 +5,44 @@
 This is an implementation of the cache that we could use to better keep
 all of the strings we are looking for in a system that we could easily
 adapt to extract relevant information for the scoring mechanism.
-The cache consists of two parts: a collection of entries, where
-an entry is a (bit string, score, and phase). Several caches may
-be instantiated with different length of bitstrings
-One would look over to the cache to calculate the score
+An 8 bit string has 256 different possible combinations
+The sum of all combinations less than 256 is 255. Therefore, we could
+introduce one 8 bit string to represent the least common
+8 bit string, and compress all other 8 bit strings into 7, 6, 5 ... or 1
+bit strings.
 
 */
 
-class Entry {
+class Cache {
 public:
-	bitstring binary;
-	unsigned long length;
-	Entry() {}
-	Entry(bitstring string) : binary(string), length(string.size()) {}
-	bool operator==(const Entry &a) {
-		if (a.length!=length) return false;
-		for (int i = 0; i<length; i++) {
-			if (a.binary[i]!=binary[i]) return false;
+	std::vector<bitstring> eightbitcombinations;
+	std::vector<int> numrepeats;
+	std::vector<bitstring> nbitkeys;
+	File t;
+	Cache(File a) : t(a) {}
+	Cache() {}
+	
+	bool areEqual(bitstring a, bitstring b) {
+		if (a.size()!=b.size()) return false;
+		for (int i = 0; i<a.size(); i++) {
+			if (a[i]!=b[i]) return false;
 		}
 		return true;
 	}
-};
-struct EntryPair {
-	bool identical; //this means the pairs themselves are identical
-	Entry key, two;
-	unsigned long length;
-	EntryPair() {}
-	EntryPair(Entry o, Entry t) : key(o), two(t), length(o.length) {
-		if (o==t) identical = true;
-		else identical = false;
+	unsigned long getNumReps(File t, bitstring find) {
+		int count = 0;
+		for (int i = 0; i<t.length; i+=find.size()) {
+			if (areEqual(t.substr(i, i+9), find)) count++;
+		}
+		return count;
 	}
-	bool isReverseOf(EntryPair a) {
-		//not reverse bits, reverse order of being saved!
-		if (a.key == two && a.two == key) return true;
-		return false; //this means the two pairs being considered are exact copies of each other
-
-	}
-	bool operator==(EntryPair &a) {
-		//no idea why a is not declared const--compiler gives an error
-		if (a.key == key && a.two == two) return true;
-		return false;
-	}
-};
-class Cache {
-public:
-	unsigned long length;
-	unsigned long keylength;
-	std::vector<EntryPair> entries;
-	Cache() : length(0) {}
-	Cache(std::vector<EntryPair> e) : length(e[0].length) {
-		keylength = length+2; // b1, b2, key
-	}
-	void addPair(EntryPair e) {
-		entries.push_back(e); length++; keylength++;
-	}
-	bool contains(EntryPair e) {
-		for (int i = 0; i<entries.size(); i++) {
-			if (e==entries[i]) return true;
+	bool contains(File a, bitstring sub) {
+		if (a.length<sub.size()) return false;
+		for (int i = 0; i<a.length; i++) {
+			if (a[i]==sub[i] && a[i+1]==sub[i+1]) {
+				if (areEqual(a.substr(i,i+sub.size()), sub)) return true;
+			}
 		}
 		return false;
-	}
-	bitstring compress() {
-		bitstring resultant;
-		//first bit tells us whether the bits are equal
-		//second bit tells us which of the two strings are being used as a key
-		//then fill up the bitstring with the appropriate amount of repeats
-		//in their correct indices.
-		return resultant;
 	}
 };

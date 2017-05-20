90101b8e1203ffc505a26e735d723a655f65b46a
Linus <llrastegar@users.noreply.github.com>
2017-05-20 14:33:44 -0500

    Update cache.h

--- cache.h
+++ cache.h
@@ -5,16 +5,27 @@
 This is an implementation of the cache that we could use to better keep
 all of the strings we are looking for in a system that we could easily
 adapt to extract relevant information for the scoring mechanism.
-An 8 bit string has 256 different possible combinations
-The sum of all combinations less than 256 is 255. Therefore, we could
-introduce one 8 bit string to represent the least common
-8 bit string, and compress all other 8 bit strings into 7, 6, 5 ... or 1
-bit strings.
+You can use an 11 bit key to encode 16 bits of information
+You do this by assigning an 8 bit key to two 8 bit strings
+These strings could be identical, or the reverse pair of another
+pair, and therefore get the same key. A single key also needs to
+represent some other combination or else there will not be enough
+keys to go around. The three things described above are simple
+boolean values assigned to each key which is hardcoded in both
+the compression and decompression software. As a result, you need
+keylength + 3 bits to encode information, for a total of 11 bits
+per 16. Obviously, you can compound this over and over again until
+you've compressed it n times into (11/16)^n of its original size.
+Note that this fraction becomes tiny very quickly.
 
 */
 
+bool checkLength(bitstring a, bitstring b) {
+	return a.size()==b.size();
+}
+
 bool compareBitstrings(bitstring one, bitstring two) {
-	if (one.size()!=two.size()) return false;
+	if (!checkLength(one, two)) return false;
 	for (int i = 0; i<one.size(); i++) {
 		if (one[i]!=two[i]) return false;
 	}
@@ -40,7 +51,7 @@ public:
 		return compareBitstrings(a, com.a) && compareBitstrings(b, com.b);
 	}
 	bool operator |= (const EntryPair &com) {
-		return (compareBitstrings(a, com.a) && compareBitstrings(b, com.b)) || (compareBitstrings(a, com.b) && compareBitstrings(b, com.a));
+		return *this==com && (compareBitstrings(a, com.b) && compareBitstrings(b, com.a));
 	}
 	bitstring getFullBitstring(bool ord=true, bool id=false) {
 		bitstring resultant;

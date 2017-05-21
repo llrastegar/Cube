80d25803b60956f5eb49ba8f8dc1e759f16bd7d7
Linus <llrastegar@users.noreply.github.com>
2017-05-21 15:21:13 -0500

    The Pyramid of Death

--- compress.cpp
+++ compress.cpp
@@ -1,5 +1,4 @@
 #include "hardcodegen.h"
-#include "compress.h"
 
 //room for the compression algorithm and other classes
 
@@ -10,4 +9,39 @@ int main() {
 	outf("Type: ", mp3File.type);
 	outf("Size: * bytes", mp3File.length);*/
 	mapTokeymap(keymap);
+	String a = "00000000";
+	outln(a);
+	for (int i = 0; i<8; i++) {
+		a[i] = '1';
+		for (int j = 0; j<8; j++) {
+			if (i!=j) {
+				a[j]='1';
+				for (int k = 0; k<8; k++) {
+					if (k!=j && i!=k) {
+						a[k] = '1';
+						for (int l = 0; l<8; l++) {
+							if (l!=k && l!=j && l!=i) {
+								a[l] = '1';
+								for (int m = 0; m<8; m++) {
+									if (m!=k && m!=j && m!=i && m!=l) {
+										a[m] = '1';
+										outln(a);
+										a[m] = '0';
+									}
+								}
+								outln(a);
+								a[l] = '0';
+							}
+						}
+						outln(a);
+						a[k] = '0';
+					}
+				}
+				outln(a);
+				a[j]='0';
+			}
+		}
+		
+		a[i]='0';
+	}
 }

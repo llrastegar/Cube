d72ecd8f4b3756e51670ddf03b79c01272ab14e2
Linus <llrastegar@users.noreply.github.com>
2017-05-21 14:45:12 -0500

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -1,42 +1,13 @@
+#include "hardcodegen.h"
 #include "compress.h"
 
 //room for the compression algorithm and other classes
 
 
 int main() {
-	/*
-	File mp3File("sound.mp3");
-	outf("Name: *\n", mp3File.name);
-	//outf("Type: *\n", mp3File.type);
-	//outf("Size: * bytes\n", mp3File.length);
-	//*/
-	//*
-	File txtFile("Ideas.txt.compress");
-	outf("Name: *\n", txtFile.name);
-	outf("Type: *\n", txtFile.type);
-	outf("Size: * bytes\n", txtFile.length);
-	txtFile.open("Ideas.txt.compress");
-	//txtFile.close();
-	//*/
-
-	/*
-	for(long i=0;i<mp3File.binary.size();i++){
-		out(mp3File.binary[i]);
-	}
-	outlnend("");
-	return 0;
-	//*/
-	
-	/*
-	bitstring seed; //the seed bitstring
-	seed.push_back(true);
-	seed.push_back(true);
-	seed.push_back(true);
-	seed.push_back(true);
-	//*/
-	/*/
-	outlnend("Finding repeating bitstrings");
-	std::vector<bitstring> v = mp3File.getRepeatingBitstrings(seed, 100);
-	//*/
-	
+	/*File mp3File("sound.mp3");
+	outf("Name: ", mp3File.name);
+	outf("Type: ", mp3File.type);
+	outf("Size: * bytes", mp3File.length);*/
+	mapTokeymap(keymap);
 }
